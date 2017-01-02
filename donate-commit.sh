721368b2c5aae54824d4ba246b661db4ee189e97
Eleven Chen <skyhacker@126.com>
2017-01-02 12:15:02 +0800

    update

--- donate-commit.sh
+++ donate-commit.sh
@@ -1,3 +1,4 @@
 git add ./
 git commit -m '又有赞助啦，oh yeah'
 git push origin master
+git push coding master
--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 git add ./
 git commit -m 'update'
 git push origin master
+git push coding master
